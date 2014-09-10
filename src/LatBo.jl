d8f7aa2458f37198d9bab41d92158a6ecb7de627
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-09-10 16:20:46 +0100

    Adding list of weights and celerities

--- src/LatBo.jl
+++ src/LatBo.jl
@@ -1,11 +1,13 @@
 module LatBo
 
-export geometry, playground, LatticeBoltzmann, SingleRelaxationTime
+export geometry, playground, LatticeBoltzmann,
+    SingleRelaxationTime, D2Q9, D3Q19
 
 abstract LatticeBoltzmann
 
 include("geometry.jl")
 include("playground.jl")
 include("single_relaxation_time.jl")
+include("kernel.jl")
 
 end # module
