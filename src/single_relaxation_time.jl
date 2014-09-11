8bc3c7ded319ac8a325b5f436e328a8289539c40
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-09-11 14:14:27 +0100

    Simplifying main data structure

--- src/single_relaxation_time.jl
+++ src/single_relaxation_time.jl
@@ -1,38 +1,26 @@
 using Base: Cartesian
 
 type SingleRelaxationTime{T <: FloatingPoint, DIMS} <: LatticeBoltzmann
-    Δt::Float64 # Time-step
-    Δx::Float64 # Lattice step
     τ⁻¹::Float64  # Inverse of the relaxation time
-    kernel::Symbol
 
     populations :: Array
     playground :: Array
 end
 
-function SingleRelaxationTime{T}(Δt::T, Δx::T, τ⁻¹::T,
-    kernel::Symbol, dimensions::(Int...))
-
-    spatial_dims, neighbors = match(
-        r"D(2|3)Q(\d+)", string(kernel)
-    ).captures |> int
-
-    @assert Δx > 0
-    @assert Δt > 0
+function SingleRelaxationTime{T}(τ⁻¹::T, dimensions::(Int...))
     @assert τ⁻¹ > 0
-    @assert spatial_dims == 2 || spatial_dims == 3
-    @assert spatial_dims == length(dimensions)
+    @assert length(dimensions) == 2 || length(dimensions) == 3
 
-    if spatial_dims == 2
+    if length(dimensions) == 2
         SingleRelaxationTime{T, 2}(
-            Δt::T, Δx::T, τ⁻¹::T, kernel,
-            zeros(T, tuple(neighbors, dimensions...)),
+            τ⁻¹::T,
+            zeros(T, tuple(9, dimensions...)),
             zeros(playground.Feature, dimensions)
         )
     else
         SingleRelaxationTime{T, 3}(
-            Δt::T, Δx::T, τ⁻¹::T, kernel,
-            zeros(T, tuple(neighbors, dimensions...)),
+            τ⁻¹::T,
+            zeros(T, tuple(19, dimensions...)),
             zeros(playground.Feature, dimensions)
         )
     end
--- test/lattice_loop.jl
+++ test/lattice_loop.jl
@@ -3,7 +3,7 @@ using LatBo: SingleRelaxationTime, lattice_loop
 
 facts("Single relaxation time initialization") do
     context("2D") do
-        sim = SingleRelaxationTime(1.5, 1.6, 0.5, :D2Q9, (5, 5))
+        sim = SingleRelaxationTime(0.5, (5, 5))
         sim.populations = rand(size(sim.populations)...)
         populations = copy(sim.populations)
         sim.playground = rand(size(sim.playground)...)
@@ -17,7 +17,7 @@ facts("Single relaxation time initialization") do
         @fact sim.populations => roughly(ones(size(populations)...))
     end
     context("3D") do
-        sim = SingleRelaxationTime(1.5, 1.6, 0.5, :D3Q3, (3, 3, 3))
+        sim = SingleRelaxationTime(0.5, (3, 3, 3))
         sim.populations = rand(size(sim.populations)...)
         populations = copy(sim.populations)
         sim.playground = rand(size(sim.playground)...)
