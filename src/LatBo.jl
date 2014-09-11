fc479492025bc90aa6bb74fec756e16082e71a37
aharwood2 <adrian.harwood@manchester.ac.uk>
2014-09-11 09:17:50 +0100

    git screw up

--- src/LatBo.jl
+++ src/LatBo.jl
@@ -1,7 +1,8 @@
 module LatBo
 
 export geometry, playground, LatticeBoltzmann,
-    SingleRelaxationTime, D2Q9, D3Q19, thermodynamics, collision
+    SingleRelaxationTime, D2Q9, D3Q19, thermodynamics, collision,
+	noslip_boundary
 
 abstract LatticeBoltzmann
 
@@ -12,5 +13,6 @@ include("collision.jl")
 include("integer_calc.jl")
 include("kernel.jl")
 include("thermodynamics.jl")
+include("noslip_boundary.jl")
 
 end # module
