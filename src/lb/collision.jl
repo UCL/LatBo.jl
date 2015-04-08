7079c260da86bd1b7d5604d328f5032160266582
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-04-08 09:43:55 +0100

    Introducing collision! that modifies pop in place

--- src/lb/collision.jl
+++ src/lb/collision.jl
@@ -5,4 +5,8 @@ end
 
 # Defines collision kernels for SRT
 collision{T}(τ⁻¹::T, fᵢ::Vector{T}, feq::Vector{T}) = τ⁻¹ * (feq - fᵢ)
-collision{T}(k::SingleRelaxationTime{T}, fᵢ::Vector{T}, feq::Vector{T}) = collision(k.τ⁻¹, feq, fᵢ)
+function collision{T}(k::SingleRelaxationTime{T}, fᵢ::Vector{T}, feq::Vector{T})
+    collision(k.τ⁻¹, feq, fᵢ)
+end
+# Applies collision operator onto fᵢ
+collision!(k::Collision, fᵢ::Vector, feq::Vector) = (fᵢ += collision(k, fᵢ, feq))
--- src/lb/lb.jl
+++ src/lb/lb.jl
@@ -36,7 +36,7 @@ function local_kernel(kernel::FluidKernel, sim::Simulation, site::Integer)
     const from = gridcoords(sim.indexing, site)
     const quantities = LocalQuantities(
         typeof(sim).parameters, from, sim.populations[:, site], sim.lattice)
-    sim.populations[:, site] = collision(kernel.collision, sim.populations[:, site], quantities.feq)
+    collision!(kernel.collision, sim.populations[:, site], quantities.feq)
     for direction in 1:length(sim.lattice.weights)
         const to = index(sim.indexing, from + sim.lattice.celerities[:, direction])
         const link = to == 0 ? NOTHING: sim.playground[to]
