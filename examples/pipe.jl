41b14d2efb518edff72dcaf706395116b8b15955
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-04-08 09:44:26 +0100

    Something still rotten in the state of this code

--- examples/pipe.jl
+++ examples/pipe.jl
@@ -1,50 +1,57 @@
-using LatBo
+using LatBo: SandBox, run!, Playground, geometry, Simulation
+using LatBo.Indices: gridcoords
+using LatBo.LB: FluidKernel, SingleRelaxationTime, ParabolicVelocityIOlet, NashZeroOrderPressure
 using Gadfly
+import LatBo.LB.velocity
 
 
-sim = SingleRelaxationTime(1., (20, 20))
-sim.inlet_velocity = [.1, 0]
-playground.initialize(sim.playground) do i, j
+println(1)
+sim = SandBox(:D2Q9, (40, 40), ρ₀=1, ν₀=[1, 0])
+println(2)
+streamers = {
+    Playground.INLET => ParabolicVelocityIOlet{Float64}([1, 0], [20, 20], 20, 1),
+    Playground.OUTLET => ParabolicVelocityIOlet{Float64}([1, 0], [20, 20], 20, 0.4)
+}
+
+sim.kernels = {
+    Playground.FLUID => FluidKernel(SingleRelaxationTime{Float64}(1.), streamers)
+}
+println(3)
+Playground.initialize(sim.playground) do i, j
     coords = Float64[i, j]
-    if !geometry.is_in_pipe(coords, Float64[0, 1], Float64[10.5, 10.5], 8.)
-        return playground.SOLID
+    if !geometry.is_in_pipe(coords, Float64[0, 1], Float64[20.5, 20.5], 18.)
+        return Playground.SOLID
     elseif geometry.is_in_half_space(coords, Float64[0, -1], Float64[0, 1])
-        return playground.INLET
-    elseif geometry.is_in_half_space(coords, Float64[0, 1], Float64[0, 20])
-        return playground.OUTLET
+        return Playground.INLET
+    elseif geometry.is_in_half_space(coords, Float64[0, 1], Float64[0, 40])
+        return Playground.OUTLET
     else
-        return playground.FLUID
+        return Playground.FLUID
     end
 end
 
-lattice_loop(sim) do indices, populations, feature
-    sim.populations[:, indices...] = LatBo.initial_probability(
-       feature, [.1, 0], 5.,
-       sim.kernel.celerities, sim.kernel.weights, sim.kernel.speed_of_sound
-    )
-end
 
-function velocity(sim::LatticeBoltzmann)
+function velocity(sim::Simulation)
     velocities = zeros(size(sim.playground)..., 2)
-    lattice_loop(sim) do indices, fᵢ, feature
-        if feature != playground.SOLID
-            velocities[indices..., :] = thermodynamics.velocity(
-                fᵢ, sim.kernel.D2Q9.celerities)
+    for (i, feature) in enumerate(sim.playground)
+        if feature == Playground.FLUID
+            velocities[gridcoords(sim.indexing, i)..., :] =
+                velocity(sim.populations[:, i], sim.lattice.celerities)
         else
-            velocities[indices..., :] = [-0, -0]
+            velocities[gridcoords(sim.indexing, i)..., :] = [0, 0]
         end
     end
     velocities
 end
 
+println(4)
+run!(sim, nsteps=20)
+println(5)
 alls = Any[]
 push!(alls, velocity(sim))
-LatBo.run_lb(sim, 5) do
-    push!(alls, velocity(sim))
-end
 npoints = size(sim.playground, 1)
 plot(
     x=repeat([1:npoints...], inner=[npoints]),
     y=repeat([1:npoints...], outer=[npoints]),
-    color=alls[1]
+    color=alls[1][:, :]
 )
