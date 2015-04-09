9f698a8020e2276b35a8f44272eb6557bd886bfb
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-04-09 20:04:34 +0100

    Adding other iolets... no unit tests yet

--- src/lb/iolet.jl
+++ src/lb/iolet.jl
@@ -21,9 +21,8 @@ end
 streaming(
     streamer::VelocityIOlet, quantities::LocalQuantities, sim::Simulation,
     from::Integer, to::Integer, direction::Integer
-) = streaming(streamer, sim, from, to, direction)
-function streaming(
-        streamer::VelocityIOlet, sim::Simulation, from::Integer, to::Integer, direction::Integer)
+) = streaming(streamer, sim, from, direction)
+function streaming(streamer::VelocityIOlet, sim::Simulation, from::Integer, direction::Integer)
     const bb_direction = sim.lattice.inversion[direction]
     const cᵢ = sim.lattice.celerities[:, direction]
     const wᵢ = sim.lattice.weights[direction]
@@ -42,8 +41,22 @@ function velocity(iolet::ParabolicVelocityIOlet, position)
     ((1 - radial) * iolet.maxspeed) * iolet.normal
 end
 
+type ConstantVelocityIOlet{T <: FloatingPoint} <: VelocityIOlet
+    velocity :: Vector{T}
+end
+streaming(
+    streamer::ConstantVelocityIOlet, quantities::LocalQuantities, sim::Simulation,
+    from::Integer, to::Integer, direction::Integer
+) = streaming(streamer, sim, from, direction)
+function streaming(iolet::VelocityIOlet, sim::Simulation, from::Integer, direction::Integer)
+    const bb_direction = sim.lattice.inversion[direction]
+    const cᵢ = sim.lattice.celerities[:, direction]
+    const wᵢ = sim.lattice.weights[direction]
+    const correction = 2wᵢ * dot(cᵢ, iolet.velocity) / speed_of_sound_squared
+    sim.next_populations[bb_direction, from] = sim.populations[direction, from] - correction
+end
 
-abstract PressureIOlet{T} <: IOLetStreaming
+
 type NashZeroOrderPressure{T} <: IOLetStreaming
     # Direction of the inlet
     normal :: Vector{T}
@@ -69,4 +82,25 @@ function streaming(
     const wᵢ = sim.lattice.weights[invdir:invdir]
     const feq = equilibrium(iolet.density, iolet_μ, cᵢ, wᵢ)[1]
     sim.next_populations[invdir, from] = feq
+end
+
+type ConstantPopulationIOlet{I <: Integer} <: IOLetStreaming
+    # Normal to iolet
+    # Should be a lattice vector
+    # Should points from fluid site to iolet site
+    normal :: Vector{I}
+end
+
+function streaming(
+    iolet::ConstantPopulationIOlet, quantities::LocalQuantities, sim::Simulation,
+    from::Integer, to::Integer, direction::Integer)
+    # This object can stream to a different object than expected
+    const invdir = sim.lattice.inversion[direction]
+    const mirror = gridcoords(sim.indexing, from) + iolet.normal + sim.lattice.celerities[:, invdir]
+    const mirror_site = index(sim.indexing, mirror)
+    const link = to == 0 ? NOTHING: sim.playground[mirror_site]
+    const this_kernel = sim.kernels[sim.playground[from]]
+    const streamer = get(this_kernel.streamers, link, NULLSTREAMER)
+    @assert !(typeof(streamer) <: IOLetStreaming)
+    streaming(streamer, quantities, sim, from, mirror_site, invdir)
 end
